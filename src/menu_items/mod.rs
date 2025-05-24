73361ad84eb9c963aaf1dfab99d55a0c1c824ea0
Adrian Tello Lasheras <adriantl7@gmail.com>
2025-05-24 22:55:15 +0200

    Prepare menu item features

--- src/menu_items/mod.rs
+++ src/menu_items/mod.rs
@@ -1,7 +1,15 @@
+#[cfg(feature = "action_menu_item")]
 pub mod action_menu_item;
+
+#[cfg(feature = "basic_menu_item")]
 pub mod basic_menu_item;
+
+#[cfg(feature = "list_menu_item")]
 pub mod list_menu_item;
 pub mod menu_item;
+
+#[cfg(feature = "range_menu_item")]
 pub mod range_menu_item;
-pub mod submenu_menu_item;
+
+#[cfg(feature = "toggle_menu_item")]
 pub mod toggle_menu_item;
